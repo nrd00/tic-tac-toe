5c5d29b9c316bad52a2f20c47d8d4fd626056569
Naimur Rahman Dorjoy <rahmandurjoy9@gmail.com>
2025-06-26 02:15:04 +0600

    Update App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Board from "./Components/Board";
 import "./App.css";
+import Game from "./Components/Game";
 
 const App = () => {
   return (
     <div className="main">
       <div>
-        <Board />
+        <Game />
       </div>
     </div>
   );
